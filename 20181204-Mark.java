b83de43cb916f3ddc7e4209558fdf2f6394335d9
Mark <45356695+z-Phoenix-z@users.noreply.github.com>
2018-12-04 09:28:46 +0800

    Update 20181204-Mark.java

--- 20181204-Mark.java
+++ 20181204-Mark.java
@@ -4,11 +4,12 @@
 public class Hiddenword(){
 private String puzzle;
 
-public Hiddenword (){}
-
+public Hiddenword (String a){
+  puzzle = a;
+}
 
 public String getHint (String guess){
-String a = new String(puzzle.length()); 
+String a = "";
 for (int i = 0; i < guess.length();i++){
   if (puzzle.substring(i,i+1).equalsTo(guess.substring(i,i+1))){
   a.substring(i,i+1) = guess.substring(i,1+1);
@@ -16,9 +17,9 @@ for (int i = 0; i < guess.length();i++){
   else if (puzzle.indexOf(guess.substring(i,i+1)) != -1){
   a.substring(i,i+1) = "+"
   }
-  else if (puzzle.indexOf(guess.substring(i,i+1)) == -1){
+  else{
   a.substring(i,i+1) = "*"
   }
-  return a;
 }
+  return a;
 }
